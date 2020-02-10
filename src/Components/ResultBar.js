fb470b75daf55e9f4ca058f08a197089638e52a3
solariswu <solariswu@hotmail.com>
2020-02-10 16:56:11 +1100

    result bar pagnation display

--- src/Components/ResultBar.js
+++ src/Components/ResultBar.js
@@ -11,14 +11,17 @@ export const ResultBar = (props) => {
         return (<div></div>);
 
     let results = props.results;
+    let offset = 0;
 
     if (results.length > MAX_RESULT_DISPLAY_IN_BAR) {
         let centerPoint = props.results.indexOf('-');
-        if (centerPoint > MAX_RESULT_DISPLAY_IN_BAR) 
+        if (centerPoint >= MAX_RESULT_DISPLAY_IN_BAR) {
+            offset = centerPoint;
             if (centerPoint + MAX_RESULT_DISPLAY_IN_BAR > results.length)
                 results = props.results.slice(centerPoint);
             else
                 results = props.results.slice(centerPoint, MAX_RESULT_DISPLAY_IN_BAR+centerPoint);
+        }
         else
             results = props.results.slice(0, MAX_RESULT_DISPLAY_IN_BAR);
     }
@@ -35,7 +38,7 @@ export const ResultBar = (props) => {
                                                        key={index}
                                                        className="mr-1"
                                                        >
-                                                           {index+1}
+                                                           {index+1+offset}
                                                        </Button>) }
             </ButtonGroup>
         </div>
