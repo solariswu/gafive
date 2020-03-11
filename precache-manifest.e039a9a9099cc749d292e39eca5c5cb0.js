00d89c4b647a3c90ad0401f5cf843b638db6ac1b
solariswu <solariswu@hotmail.com>
2020-03-11 09:07:20 +0000

    Deploying to gh-pages from master 1f37e68289e89c8236f0c5fd28faa34fba28dc46

---
+++ precache-manifest.e039a9a9099cc749d292e39eca5c5cb0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a63d8e7e2ff54cf921ce65d60790fc37",
+    "url": "/gafive/index.html"
+  },
+  {
+    "revision": "71c7cd54bb0cbe8d072f",
+    "url": "/gafive/static/css/2.51aad67d.chunk.css"
+  },
+  {
+    "revision": "1201a02fabb110c8576b",
+    "url": "/gafive/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "71c7cd54bb0cbe8d072f",
+    "url": "/gafive/static/js/2.694587b3.chunk.js"
+  },
+  {
+    "revision": "d004930eacd4a4dd76df6330bca8699b",
+    "url": "/gafive/static/js/2.694587b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1201a02fabb110c8576b",
+    "url": "/gafive/static/js/main.fa8f056d.chunk.js"
+  },
+  {
+    "revision": "67b9a51fd8e2c6a33bdd",
+    "url": "/gafive/static/js/runtime-main.bc18fd04.js"
+  },
+  {
+    "revision": "8e3d6124f258a1690567f02ade38cd2d",
+    "url": "/gafive/static/media/correct.8e3d6124.mp3"
+  },
+  {
+    "revision": "43b2d48724e93920a594cb4b8c48cbc6",
+    "url": "/gafive/static/media/wrong.43b2d487.wav"
+  }
+]);
